78e09efc5eb06c7f8375de8661909d7ae6358ef9
Alexandrdrdrd <kands12349@gmail.com>
2022-12-01 13:23:15 +0200

    addded user entity

---
+++ src/main/java/com/sahsa/rest/webservices/restfulwebservices/user/UserDAOService.java
@@ -0,0 +1,14 @@
+package com.sahsa.rest.webservices.restfulwebservices.user;
+
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserDAOService {
+
+    private static List<User> users = new ArrayList<>();
+
+}
