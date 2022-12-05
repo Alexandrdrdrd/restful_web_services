ffadd5741e5670b96424741c3f4ef4aa447ae0dc
Alexandrdrdrd <kands12349@gmail.com>
2022-12-05 15:58:13 +0200

    connected to h2 DB

--- src/main/java/com/sahsa/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/sahsa/rest/webservices/restfulwebservices/user/User.java
@@ -1,15 +1,20 @@
 package com.sahsa.rest.webservices.restfulwebservices.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
-
+@Entity(name = "user_details")
 public class User {
 
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min = 2, max = 20, message = "name should be between 2 and 20 characters")
     @JsonProperty("user_name")
@@ -45,6 +50,9 @@ public class User {
         this.id = id;
         this.name = name;
         this.birthday = birthday;
+    }
+
+    public User() {
     }
 
     @Override
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into user_details(id,birthday,name)
+values (1001,current_date(), 'sasha');
+
+insert into user_details(id,birthday,name)
+values (1002,current_date(), 'misha');
+
+insert into user_details(id,birthday,name)
+values (1003,current_date(), 'dima');
